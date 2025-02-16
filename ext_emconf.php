7806a48c431eefe49a1c5545d9d5d8922bd7a84c
Régis Tedone <regis.tedone@ideative.ch>
2025-02-16 11:17:48 +0100

    Initial commit

---
+++ ext_emconf.php
@@ -0,0 +1,18 @@
+<?php
+/** @var $_EXTKEY */
+$EM_CONF[$_EXTKEY] = [
+    'title' => 'html5 self-closing tags patcher',
+    'description' => 'This extension patches TYPO3 to eliminate non-HTML5-standard self-closing tags.',
+    'category' => 'misc',
+    'author' => 'Regis TEDONE',
+    'state' => 'stable',
+    'clearCacheOnLoad' => 1,
+    'version' => '12.0.0',
+    'constraints' => [
+        'depends' => [
+            'typo3' => '12.4.0-12.4.99',
+        ],
+        'conflicts' => [],
+        'suggests' => [],
+    ],
+];
